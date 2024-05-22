320b6077617d052b973a63746e1cee3a43a51953
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-05-22 12:01:08 +0500

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("body");
+let taxminiyson = Math.floor(Math.random() * 100);
+let h1 = document.createElement("h1");
+
+h1.textContent = taxminiyson;
+h1.style = "text-align: center";
+body.appendChild(h1);
